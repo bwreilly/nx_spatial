9aae452793b88748ce71ab0f2d256527311ff484
Ben Reilly <gallipoli@gmail.com>
2011-01-20 22:25:11 -0800

    tests added for flow_errors

--- nx_spatial/algorithms/flow_errors.py
+++ nx_spatial/algorithms/flow_errors.py
@@ -5,10 +5,10 @@ def flow_errors(G, src, stopnodes=None):
     implicit in defined source node.
     """
     badedges = []
-    gnodes = trace(src, stopnodes)
+    gnodes = trace(G, src, stopnodes)
     #ud = self.to_undirected()
     #connected = networkx.dfs_preorder(src)
-    for edge in self.edges(): #todo: limit to potentially connected
+    for edge in G.edges(): #todo: limit to potentially connected
         start = edge[0]
         end = edge[1]
         if end in gnodes and start not in gnodes:
