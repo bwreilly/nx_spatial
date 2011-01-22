15af9095fec47c6b32ff26809f748142d96be55c
Ben Reilly <gallipoli@gmail.com>
2011-01-22 15:36:03 -0800

    fixed names on tests, new simple test for trace

--- nx_spatial/algorithms/tests/test_trace.py
+++ nx_spatial/algorithms/tests/test_trace.py
@@ -3,9 +3,9 @@ from nose import SkipTest
 from nose.tools import assert_equal
 
 import networkx as nx
-from nx_spatial import flow_errors
+from nx_spatial import trace
 
-class TestFlow_Errors(object):   
+class Test_Trace(object):   
     def setUp(self):
         self.di = nx.DiGraph()
         self.di.add_path([(7.0, 1.0), (1.0, 1.0), (2.0, 2.0), (3.0, 3.0)])
@@ -14,20 +14,9 @@ class TestFlow_Errors(object):
         self.di.add_path([(6.0, 3.0), (7.0, 7.0)])
         self.assertEqual = assert_equal
         
-    def testflow_errors_oneedgegroup(self):
-        src = ((1,1))
-        badline1 = ((4,1), (2,2))
-        badline2 = ((7,1), (1,1))
-        expected = [badline1, badline2]
-        actual = flow_errors(self.di, src)
-        assert self.sequence_equal(actual, expected)
-    
-    def testflow_errors_twoedgegroup(self):
-        src = ((2.0, 2.0))
-        badline1 = ((4,1), (2,2))
-        badline2 = ((1,1), (2,2))
-        expected = [badline1, badline2]
-        actual = flow_errors(self.di, src)
+    def test_stops(self):
+        expected = [(7.0, 1.0), (1.0, 1.0), (2.0, 2.0)]
+        actual = trace(self.di, (7.0, 1.0), [(2.0, 2.0)])
         assert self.sequence_equal(actual, expected)
     
     def sequence_equal(self, seq1, seq2, msg=None):
